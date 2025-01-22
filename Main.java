36e52ab8599d94c1114b578b8e65a242a64b3881
Rithikaa <rithikaaredde0103@gmail.com>
2025-01-22 22:50:38 +0530

    changes made

--- Main.java
+++ Main.java
@@ -1,4 +1,59 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calc = new Calculator();
+        UserInput input = new UserInput();  // Fixed class name from Input to UserInput
+
+        while (true) {
+            System.out.println("\nMenu");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Fibonacci series");
+            System.out.println("6. Sum of array");
+            System.out.println("7. Mean of array");
+            System.out.println("8. Mode of array");
+            System.out.println("9. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1: // Addition
+                    System.out.println("Sum is: " + calc.calculateSum(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 2: // Subtraction
+                    System.out.println("Difference is: " + calc.calculateDiff(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 3: // Multiplication
+                    System.out.println("Product is: " + calc.calculateProduct(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 4: // Division
+                    System.out.println("Quotient is: " + calc.calculateQuotient(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 5: // Fibonacci
+                    System.out.print("Enter the number of terms for Fibonacci series: ");
+                    int n = scanner.nextInt();
+                    calc.fibonacci(n);
+                    break;
+                case 6: // Sum of Array
+                    System.out.println("Sum of array is: " + calc.sumOfArray(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 7: // Mean of Array
+                    System.out.println("Mean of array is: " + calc.meanOfArray(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 8: // Mode of Array
+                    System.out.println("Mode of array is: " + calc.modeOfArray(input.getArrayInput()));  // Fixed method call
+                    break;
+                case 9: // Exit
+                    System.out.println("Exiting the program. Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        }
     }
 }
--- UserInput.java
+++ UserInput.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class UserInput {
+    // Get the calculation choice from the user
+    public int getCalculationChoice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Fibonacci Sequence");
+        System.out.println("6. Mean of Array");
+        System.out.println("7. Variance of Array");
+        return scanner.nextInt();
+    }
+
+    // Get array input from the user
+    public int[] getArrayInput() {  // Fixed method signature to return int[] (matching Calculator)
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];  // Array should be of type int[] (based on Calculator methods)
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();  // Corrected to nextInt()
+        }
+        return numbers;
+    }
+
+    // Get a single number input from the user
+    public int getSingleNumberInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        return scanner.nextInt();
+    }
 }
